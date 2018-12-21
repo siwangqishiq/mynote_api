916a1bb8ee712b4bcdd89404b9fed67d89ca0ace
panyi <525647740@qq.com>
2018-12-21 23:52:18 +0800

    Initial commit

---
+++ src/main/java/com/xinlan/mynnote/MainApplication.java
@@ -0,0 +1,9 @@
+package com.xinlan.mynnote;
+
+import org.springframework.boot.SpringApplication;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ src/main/java/com/xinlan/mynnote/controller/VersionController.java
@@ -0,0 +1,9 @@
+package com.xinlan.mynnote.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+
+
+}
